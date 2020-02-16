e510905f5326d3c185d27f6e76ea84a4e2302b9b
Aleksandr Gromov <Aleksaaa555@gmail.com>
2020-02-17 01:21:14 +0400

    new

--- src/algorithms/MainClass.java
+++ src/algorithms/MainClass.java
@@ -1,5 +1,9 @@
 package algorithms;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 /**
  * Created by User on 22.04.2015.
  */
@@ -215,16 +219,287 @@ public class MainClass {
         System.out.println("end towers of Hanoi app");
 
         System.out.println("begin mergesort");
-        int[] a = {1,8,9};
-        int[] b = {10,88,99};
+        int[] a = {1, 8, 9};
+        int[] b = {10, 88, 99};
         int[] c = new int[6];
-        merge(a,a.length,b,b.length,c);
+        merge(a, a.length, b, b.length, c);
         for (int i1 : c) {
             System.out.println(i1);
         }
 
         System.out.println("end mergesort");
 
+        System.out.println(1 + 1);
+
+
+        class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+
+            TreeNode(int x) {
+                val = x;
+            }
+        }
+        class ListNode {
+            int val;
+            ListNode next;
+
+            ListNode(int x) {
+                val = x;
+                next = null;
+            }
+        }
+
+        class Solution {
+            public boolean isBalanced(TreeNode root) {
+                if (root == null) return true;
+
+                TreeNode l = root.left;
+                TreeNode r = root.right;
+
+                int lDepth = traverse(l);
+                int rDepth = traverse(r);
+                int differ = lDepth - rDepth;
+                return Math.abs(differ) <= 1 && isBalanced(l) && isBalanced(r);
+            }
+
+            public int traverse(TreeNode root) {
+                if (root == null) return 0;
+
+                return 1 + Math.max(traverse(root.left), traverse(root.right));
+            }
+
+            public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+                ListNode prevA = null;
+                ListNode prevB = null;
+                boolean end = false;
+
+                while (true) {
+                    if (end) {
+                        if (headA == headB) {
+                            if (prevA == null && prevB == null) return headA;
+                            else if (prevA == null) return headB;
+                            else if (prevB == null) return headA;
+                            else {
+                                headA = prevA;
+                                headB = prevB;
+                            }
+
+
+                        }
+                    } else {
+                        if (headA.next == null && headB.next == null) {
+                            end = true;
+                            if (headA == headB) {
+                                headA = prevA;
+                                headB = prevB;
+
+                            } else return null;
+                        } else if (headA.next == null) {
+                            prevB = headB;
+                            headB = headB.next;
+                        } else if (headB.next == null) {
+                            prevA = headA;
+                            headA = headA.next;
+                        } else {
+                            prevA = headA;
+                            headA = headA.next;
+                            prevB = headB;
+                            headB = headB.next;
+                        }
+                    }
+
+
+                }
+            }
+
+            public int[] twoSum(int[] numbers, int target) {
+                if (numbers.length == 0) return new int[0];
+                if (numbers.length == 1) return new int[0];
+
+                int start = 0;
+                int end = numbers.length - 1;
+
+                while (true) {
+                    int median = (start + end) / 2;
+
+                    if (start == numbers.length) return new int[0];
+                    if (end == -1) return new int[0];
+
+                    if (start > end) {
+                        start = 0;
+                        if (end == 0) return new int[0];
+                        else break;
+                    }
+
+                    if (numbers[median] == target) {
+                        start = 0;
+                        end = median - 1;
+                        if (end == 0) return new int[0];
+                        else break;
+                    } else if (numbers[median] < target) {
+                        start = median + 1;
+                    } else if (numbers[median] > target) {
+                        end = median - 1;
+                    }
+                }
+
+                int[] result = new int[2];
+
+                int prev = numbers[start];
+
+
+                for (int i = start + 1; i <= end; i++) {
+                    int sum = prev + numbers[i];
+
+                    if (sum == target) {
+                        result[0] = i;
+                        result[1] = i + 1;
+
+                        return result;
+                    }
+
+                    if (prev > 0) {
+                        int offset = i - 2;
+                        while (offset >= 0) {
+                            sum = numbers[offset] + numbers[i];
+
+                            if (sum == target) {
+                                result[0] = i;
+                                result[1] = i + 1;
+
+                                return result;
+                            }
+                            offset--;
+                        }
+                    }
+
+                    prev = numbers[i];
+                }
+
+                return new int[0];
+            }
+
+            public String convertToTitle(int n) {
+                StringBuilder sb = new StringBuilder();
+                do {
+                    int bit1 = n % 26;
+                    int bit2 = n / 26;
+                    if (bit1 == 0) {
+                        bit1 += 26;
+                        bit2--;
+                    }
+                    sb.append((char) (bit1 + 64));
+                    n = bit2;
+                } while (n > 0);
+                return sb.reverse().toString();
+            }
+
+        }
+        class MinStack {
+
+            /** initialize your data structure here. */
+            LinkedList<Integer> stack = new LinkedList();
+            int min;
+
+            public MinStack() {
+
+            }
+
+            public void push(int x) {
+                if (stack.size() == 0) min = x;
+                stack.addFirst(x);
+                if (x < min) min = x;
+
+            }
+
+            public void pop() {
+                int removed = stack.removeFirst();
+                if (removed == min) {
+                    ListIterator<Integer> iterator = stack.listIterator();
+                    min = iterator.hasNext() ? iterator.next() : 0;
+
+                    while (iterator.hasNext()) {
+                        Integer element = iterator.next();
+                        if (element < min) min = element;
+                    }
+                }
+            }
+
+            public int top() {
+                return stack.getFirst();
+            }
+
+            public int getMin() {
+                return min;
+            }
+        }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+
+
+        Solution solution = new Solution();
+        boolean balanced = solution.isBalanced(root);
+        System.out.println(balanced);
+        MinStack minStack = new MinStack();
+        minStack.push(1);
+        minStack.push(2);
+        minStack.top();
+        minStack.getMin();
+        minStack.pop();
+
+        ListNode listNode1 = new ListNode(4);
+        ListNode listNode2 = new ListNode(1);
+        ListNode listNode3 = new ListNode(8);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(5);
+        ListNode listNode7 = new ListNode(0);
+        ListNode listNode8 = new ListNode(1);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode6.next = listNode7;
+        listNode7.next = listNode8;
+        listNode8.next = listNode3;
+//        ListNode intersectionNode = solution.getIntersectionNode(listNode1, listNode6);
+//        System.out.println(intersectionNode.val);
+
+        int[] arr3 = new int[2];
+        int target = -1;
+
+        arr3[0] = -1;
+        arr3[1] = 0;
+
+        int[] ints = solution.twoSum(arr3, target);
+        System.out.println("ints: " + Arrays.toString(ints));
+
+        System.out.println(solution.convertToTitle(27));
+
+        char[] chars = "BBB".toCharArray();
+        int n = 0;
+        int j = 0;
+        for (int i1 = chars.length - 1; i1 >= 0; i1--) {
+            n += Math.pow(26, i1) * (chars[j] - 64);
+            j++;
+        }
+
+        System.out.println(n);
+
+        quickSort();
     }
 
     private static boolean checkForDuplicate(long[] array, long value) {
@@ -266,5 +541,45 @@ public class MainClass {
         }
 
 
+    }
+
+    public static void quickSort() {
+        int[] arr = {1, 5, 3, 4, -1, 100, 99, -500, -350, -520, 1000, 999, 630, 752, 777};
+        recQuickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void recQuickSort(int[] arr, int start, int end) {
+        if ((end - start) <= 0) {
+            return;
+        } else {
+            int median = arr[end];
+            int i = partitionIt(arr, start, end, median);
+            recQuickSort(arr, start, i - 1);
+            recQuickSort(arr, i + 1, end);
+        }
+    }
+
+    private static int partitionIt(int[] arr, int start, int end, int median) {
+        int i = start - 1;
+        int j = end;
+
+        while (true) {
+            while (arr[++i] < median) {
+            }
+            while (--j >= 0 && arr[j] > median) {
+            }
+
+            if (i >= j) {
+                int temp = arr[end];
+                arr[end] = arr[i];
+                arr[i] = temp;
+                return i;
+            }
+
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
